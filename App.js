9014f5eae0a7296e9641516ebe024b838fc4b050
NateThurmond <nathanthurmond@yahoo.com>
2024-11-28 04:22:42 -0600

    Get working using latest RN version

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
-/**
- * SuperShop sample react-native application
- * https://github.com/NateThurmond/SuperShop
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   Platform,
   StyleSheet,
@@ -12,132 +6,102 @@ import {
   View,
   TextInput,
   Button,
-  AsyncStorage,
-  Image
+  Image,
+  TouchableOpacity,
 } from 'react-native';
-import { COLOR, ThemeProvider, Toolbar } from 'react-native-material-ui';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {red} from '@mui/material/colors';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
 
-const uiTheme = {
+const theme = createTheme({
   palette: {
-      primaryColor: COLOR.blue600,
+    primary: {
+      main: red[500],
+    },
   },
-  toolbar: {
-      container: {
-          height: 50,
-      },
-  },
-};
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      loggedIn:false,
-      userName:'',
-      groupNumber:''
-    }
+      loggedIn: false,
+      userName: '',
+      groupNumber: '',
+    };
 
-    // THIS IS FOR TESTING TO REMOVE ASYNC STORAGE (SHOW LOGIN PAGE) UNTIL I SETUP LOGOUT FUNCTIONALITY
+    // AsyncStorage operations
     AsyncStorage.removeItem('userName');
-
-    AsyncStorage.getItem('userName').then((userNameStr)=>{
+    AsyncStorage.getItem('userName').then(userNameStr => {
       if (userNameStr != null) {
         const userName = JSON.parse(userNameStr);
         this.setState({
-          userName:userName.userName,
-          loggedIn:true
+          userName: userName.userName,
+          loggedIn: true,
         });
       }
     });
   }
+
   signIn(userName, pass) {
-    var obj = this;
-
-    // Do database check, get group number - TODO
-
-    AsyncStorage.setItem("userName", JSON.stringify({'userName':userName}));
-
-    obj.setState({
-      userName:userName,
-      loggedIn:true
+    AsyncStorage.setItem('userName', JSON.stringify({userName}));
+    this.setState({
+      userName: userName,
+      loggedIn: true,
     });
   }
+
   signUp(userName, pass) {
-
-    // Do database check, get group number - TODO
-    // Set async storage and refresh state
-
-    alert(userName + " " + pass);
+    alert(userName + ' ' + pass);
   }
+
   render() {
     return (
-      <ThemeProvider uiTheme={uiTheme}>
+      <ThemeProvider theme={theme}>
         <View style={styles.container}>
-
           <MenuBarNotLoggedIn />
-
-          {/*<Text style={styles.instructions}>{instructions}</Text>*/}
-
-          {!this.state.loggedIn && 
-            <LoginBox signIn={this.signIn.bind(this)} signUp={this.signUp.bind(this)} userName={this.state.userName}/>
-          }
-          {this.state.loggedIn && 
-            <Text>Welcome {this.state.userName}</Text>
-          }
+          {!this.state.loggedIn && (
+            <LoginBox
+              signIn={this.signIn.bind(this)}
+              signUp={this.signUp.bind(this)}
+              userName={this.state.userName}
+            />
+          )}
+          {this.state.loggedIn && <Text>Welcome {this.state.userName}</Text>}
         </View>
       </ThemeProvider>
     );
   }
 }
 
-/*
-  MAY ADD SEARCHABLE MENU BAR SELECTION BACK IN LATER
-
-      return (
-      <Toolbar
-        leftElement="home"
-        rightElement="menu"
-        centerElement="Searchable"
-        searchable={{
-          autoFocus: true,
-          placeholder: 'Search'}}/>
-    );
-*/
-
 class MenuBarNotLoggedIn extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.homeButtonClick = this.homeButtonClick.bind(this);
-    this.optionsSelect = this.optionsSelect.bind(this);
-  }
-  homeButtonClick() {
+  homeButtonClick = () => {
     alert('You clicked the home button');
-  }
-  optionsSelect(event) {
+  };
+
+  optionsSelect = event => {
     if (event.index === 0) {
       alert('You clicked Item 1');
     } else if (event.index === 1) {
       alert('You clicked Item 2');
     }
-  }
+  };
+
   render() {
     return (
-      <Toolbar
-        leftElement="home"
-        onLeftElementPress={() => this.homeButtonClick()}
-        onRightElementPress={(event) => this.optionsSelect(event)}
-        rightElement={{
-          menu: { labels: ['Item 1', 'Item 2'] },
-        }} />
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={this.homeButtonClick}
+          style={styles.leftButton}>
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
+        <Text style={styles.title}>SuperShop</Text>
+        <TouchableOpacity
+          onPress={this.optionsSelect}
+          style={styles.rightButton}>
+          <Text style={styles.buttonText}>Menu</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
@@ -147,48 +111,84 @@ class LoginBox extends React.Component {
     super(props); // Will allow accessing props within constructor, may not be needed
 
     this.state = {
-      userName:'',
-      userPass:''
+      userName: '',
+      userPass: '',
     };
   }
   changeUserName(value) {
     this.setState({
-      userName:value
+      userName: value,
     });
   }
   changeUserPass(value) {
     this.setState({
-      userPass:value
+      userPass: value,
     });
   }
   render() {
     return (
-      <View style={{flex:1,width: '80%', maxWidth:'40%', justifyContent: 'space-between'}}>
-        <View style={{alignItems:'center',height:'40%',justifyContent: 'center'}}>
-          <Image source={require('./images/SuperShop.png')} style={{}}/>
+      <View
+        style={{
+          flex: 1,
+          width: '80%',
+          maxWidth: '40%',
+          justifyContent: 'space-between',
+        }}>
+        <View
+          style={{
+            alignItems: 'center',
+            height: '40%',
+            justifyContent: 'center',
+          }}>
+          <Image source={require('./images/SuperShop.png')} style={{}} />
         </View>
         <View>
-          <TextInput style={{fontSize:20}} {...this.props} editable={true} maxLength={40} 
-            placeholder={"Username"} value={this.state.userName} 
-            onChangeText={(text) => this.changeUserName(text)} />
-          <TextInput style={{fontSize:20}} {...this.props} editable={true} maxLength={40} 
-            placeholder={"Password"} value={this.state.userPass} 
-            onChangeText={(text) => this.changeUserPass(text)} secureTextEntry={true} />
+          <TextInput
+            style={{fontSize: 20}}
+            {...this.props}
+            editable={true}
+            maxLength={40}
+            placeholder={'Username'}
+            value={this.state.userName}
+            onChangeText={text => this.changeUserName(text)}
+          />
+          <TextInput
+            style={{fontSize: 20}}
+            {...this.props}
+            editable={true}
+            maxLength={40}
+            placeholder={'Password'}
+            value={this.state.userPass}
+            onChangeText={text => this.changeUserPass(text)}
+            secureTextEntry={true}
+          />
         </View>
-        <View style={{maxHeight:'30%', flex:1, flexDirection:'row', justifyContent: 'space-between'}}>
-          <View style={{width: 75, height: 50}} >
+        <View
+          style={{
+            maxHeight: '30%',
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <View style={{height: 50}}>
             <Button
-              onPress={() => this.props.signIn(this.state.userName, this.state.userPass)}
+              onPress={() =>
+                this.props.signIn(this.state.userName, this.state.userPass)
+              }
               title="Sign In"
               color="#3399ff"
-              accessibilityLabel="Sign In"/>
+              accessibilityLabel="Sign In"
+            />
           </View>
-          <View style={{width: 75, height: 50}} >
+          <View style={{height: 50}}>
             <Button
-              onPress={() => this.props.signUp(this.state.userName, this.state.userPass)}
+              onPress={() =>
+                this.props.signUp(this.state.userName, this.state.userPass)
+              }
               title="Sign Up"
               color="#3399ff"
-              accessibilityLabel="Sign Up"/>
+              accessibilityLabel="Sign Up"
+            />
           </View>
         </View>
       </View>
@@ -212,5 +212,29 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 60,
+    backgroundColor: '#1976D2', // Customizable header background color
+    paddingHorizontal: 10,
+  },
+  leftButton: {
+    padding: 10,
+  },
+  rightButton: {
+    padding: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  title: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
---
+++ ios/Pods/Target Support Files/Pods-SuperShop-SuperShopTests/Pods-SuperShop-SuperShopTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SuperShop_SuperShopTests : NSObject
+@end
+@implementation PodsDummy_Pods_SuperShop_SuperShopTests
+@end
